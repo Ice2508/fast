fca2c87f61894695f6233040d4880e5beda30491
Ice2508 <Ice2508@yandex.ru>
2025-08-02 15:05:28 +0300

    ok

---
+++ main.js
@@ -0,0 +1,75 @@
+'use strict'
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+  effect: 'cube',
+  cubeEffect: {
+  shadow: true,
+  slideShadows: true,
+  shadowOffset: 20,
+  shadowScale: 0.94,
+},
+});
+
+const callTOActionBtn = document.querySelector('.call-to-action__btn');
+const popupOverlay = document.querySelector('.popup__overlay');
+const popupForm = document.querySelector('.popup__form');
+const popupClose = document.querySelector('.popup__close');
+const popupStatus = document.querySelector('.popup__status');
+
+callTOActionBtn.addEventListener('click', () => {
+     popupOverlay.classList.add('popup__overlay--active');
+     popupForm.classList.add('popup__form--active');
+     document.documentElement.style.overflow = 'hidden';
+});
+
+popupClose.addEventListener('click', () => {
+    popupOverlay.classList.remove('popup__overlay--active');
+    popupForm.classList.remove('popup__form--active');
+    document.documentElement.style.overflow = '';
+})
+
+popupForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const formData = new FormData(popupForm);
+    try {
+      const response = await fetch('https://api.web3forms.com/submit', {
+        method: 'POST',
+        body: formData
+      });
+      
+      if(response.ok) {
+        popupStatus.innerHTML = 'отправлено успешно! &#10004;'
+        popupStatus.style.color = 'green';
+        setTimeout(() => {
+          popupOverlay.classList.remove('popup__overlay--active');
+          popupForm.classList.remove('popup__form--active');
+          document.documentElement.style.overflow = '';
+          popupStatus.textContent = '';
+          popupForm.reset();
+        }, 800);
+      } else {
+        popupStatus.textContent = 'ошибка отправки!'
+      }
+    } catch {
+      popupStatus.textContent = 'ошбика сетeвого соединения!'
+    }
+})
