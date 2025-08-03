679711212f104ac1aa46dd3b9a633567c8b08b5c
Ice2508 <Ice2508@yandex.ru>
2025-08-04 01:00:30 +0300

    ok

--- main.js
+++ main.js
@@ -4,8 +4,6 @@ const swiper = new Swiper('.swiper', {
   // Optional parameters
   direction: 'horizontal',
   loop: true,
-  touchStartPreventDefault: true,
-
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
