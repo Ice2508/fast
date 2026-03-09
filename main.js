53173c8f64d9022a3e273b3baf64a364df4d9971
Ice2508 <Ice2508@yandex.ru>
2026-03-09 13:14:17 +0300

    ok

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ AOS.init({
   offset: 100,
 });
 
-const isMobile = window.matchMedia('(max-width: 768px)').matches;
 const swiper = new Swiper('.swiper', {
   // Optional parameters
   direction: 'horizontal',
@@ -23,7 +22,8 @@ const swiper = new Swiper('.swiper', {
   scrollbar: {
     el: '.swiper-scrollbar',
   },
-  effect: isMobile ? 'fade' : 'cube',
+  effect: 'flip',
+  speed: 1500,
   cubeEffect: {
   shadow: true,
   slideShadows: true,
@@ -63,7 +63,11 @@ const callTOActionBtn = document.querySelector('.call-to-action__btn');
 const popupOverlay = document.querySelector('.popup__overlay');
 const popupForm = document.querySelector('.popup__form');
 const popupClose = document.querySelector('.popup__close');
-const popupStatus = document.querySelector('.popup__status');
+const popupStatus = document.querySelector('.popup-status');
+const popupStatusTitle = document.querySelector('.popup-status__title');
+const popupStatusSubtitle = document.querySelector('.popup-status__subtitle');
+const popupStatusIcon = document.querySelector('.popup-status__icon');
+const popupBtn = document.querySelector('.popup__btn');
 
 callTOActionBtn.addEventListener('click', () => {
      popupOverlay.classList.add('popup__overlay--active');
@@ -72,7 +76,7 @@ callTOActionBtn.addEventListener('click', () => {
 });
 
 popupOverlay.addEventListener('click', (e) => {
-    if(e.target === popupOverlay) {
+    if (e.target === popupOverlay) {
     setTimeout(() => {
       popupForm.classList.remove('popup__form--active');
       popupOverlay.classList.remove('popup__overlay--active');
@@ -82,18 +86,20 @@ popupOverlay.addEventListener('click', (e) => {
 });
 
 popupClose.addEventListener('click', () => {
-
-    setTimeout(() => {
     popupForm.classList.remove('popup__form--active');
+    setTimeout(() => {
+    
     popupOverlay.classList.remove('popup__overlay--active');
     document.documentElement.style.overflow = '';
-  },100);
+  },300);
 })
 
 
 
 popupForm.addEventListener('submit', async (e) => {
     e.preventDefault();
+    popupBtn.disabled = true;
+    popupBtn.style.opacity = 0.5;
     const formData = new FormData(popupForm);
     try {
       const response = await fetch('https://api.web3forms.com/submit', {
@@ -102,19 +108,32 @@ popupForm.addEventListener('submit', async (e) => {
       });
       
       if(response.ok) {
-        popupStatus.innerHTML = 'отправлено успешно! &#10004;'
-        popupStatus.style.color = 'green';
-        setTimeout(() => {
-          popupOverlay.classList.remove('popup__overlay--active');
           popupForm.classList.remove('popup__form--active');
-          document.documentElement.style.overflow = '';
-          popupStatus.textContent = '';
-          popupForm.reset();
-        }, 800);
+          setTimeout(() => {
+          popupStatus.classList.add('popup-status--active');
+        }, 300)
+        popupForm.reset();
       } else {
-        popupStatus.textContent = 'Ошибка отправки!'
+        popupForm.classList.remove('popup__form--active');
+        popupStatus.classList.add('popup-status--active');
+        popupStatusTitle.textContent = "Не удалось отправить сообщение!!!";
+        popupStatusSubtitle.textContent = "Попробуйте еще раз через несколько минут.";
+        popupStatusIcon.style.backgroundImage = "url(icon-form-error.webp)";
       }
     } catch {
-      popupStatus.textContent = 'Ошибка сетeвого соединения!'
+      popupStatus.classList.add('popup-status--active');
+      popupStatusTitle.textContent = "Не удалось отправить сообщение!!!";
+      popupStatusSubtitle.textContent = "Проверьте подключение к интернету и попробуйте снова.";
+      popupStatusIcon.style.backgroundImage = "url(icon-form-error.webp)"
+    } finally {
+      popupBtn.disabled = false;
+      popupBtn.style.opacity = 1;
     }
+})
+
+const popupStatusBtn = document.querySelector('.popup-status__btn');
+popupStatusBtn.addEventListener('click', () => {
+  popupStatus.classList.remove('popup-status--active');
+  popupOverlay.classList.remove('popup__overlay--active');
+  document.documentElement.style.overflow = '';
 })
